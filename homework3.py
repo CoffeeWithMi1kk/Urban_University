335300703fb38050b3c34e8b6edc9f03e577c05b
CoffeeWithMi1kk <dbjqip73@gmail.com>
2024-06-21 02:16:34 +0300

    Add files via upload
    Практическая работа по уроку "Динамическая типизация"

---
+++ homework3.py
@@ -0,0 +1,16 @@
+# Задание №1
+name = "Ilya"
+# Задание №2
+print("Name:", name)
+# Задание №3
+age = 26
+# Задание №4
+print("Age:", age)
+# Задание №5
+age = age + 1
+# Задание №6
+print("New age:", age)
+# Задание №7
+is_student = True
+# Задание №8
+print("Is Student:", is_student)
