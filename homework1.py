fb08f6e3d3b468c0372d73f5a6b982bf70ffba35
CoffeeWithMi1kk <dbjqip73@gmail.com>
2024-06-21 02:25:40 +0300

    Add files via upload
    Практическое задание по уроку "Строки и индексация строк"

---
+++ homework1.py
@@ -0,0 +1,12 @@
+#Задание №1
+example = "Identification"
+#Задание №2
+print(example[0])
+#Задание №3
+print(example[-1])
+#Задание №4
+print(example[7:])
+#Задание №5
+print(example[::-1])
+#Задание №6
+print(example[1::2])
