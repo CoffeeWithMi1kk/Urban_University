800395a5f6f44f373bedb372eab471d3d6035487
CoffeeWithMi1kk <dbjqip73@gmail.com>
2024-06-21 02:36:39 +0300

    Add files via upload
    Практическая работа по уроку "Организация программ и методы строк."

---
+++ homework4.py
@@ -0,0 +1,15 @@
+# Задание №1
+my_string = input()
+print(my_string)
+# Задание №2
+print(len(my_string))
+# Задание №3
+print(my_string.upper())
+# Задание №4
+print(my_string.lower())
+# Задание №5
+print(my_string .replace(" ", ""))
+# Задание №6
+print(my_string[0])
+# Задание №7
+print(my_string[-1])
