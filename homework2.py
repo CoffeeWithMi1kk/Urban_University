959136cbffc6c94c489a52388fa0a415e87755b0
CoffeeWithMi1kk <dbjqip73@gmail.com>
2024-06-21 02:24:25 +0300

    Add files via upload
    Практическое задание по теме "Переменные."

---
+++ homework2.py
@@ -0,0 +1,11 @@
+#Задание №1
+total_tasks = 12
+#Задание №2
+total_hours = 1.5
+#Задание №3
+course_name = "Python"
+#Задание №4
+average_time_per_task = total_hours / total_tasks
+#Задание №5
+print(f"Курс: {course_name}, всего задач:{total_tasks}, затрачено часов: {total_hours}, "
+      f"среднее время выполнения {average_time_per_task:.3f} часа.")
