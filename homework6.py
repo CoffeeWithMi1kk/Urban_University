fa61c5f3990f278e8221c708a0702cee7cd87c71
CoffeeWithMi1kk <dbjqip73@gmail.com>
2024-06-23 14:45:45 +0300

    Add files via upload
    Практическое задание по теме: "Словари и множества"

---
+++ homework6.py
@@ -0,0 +1,24 @@
+# Задание №1
+my_dict = {"Ilya": 1997, "Misha": 1996, "Igor": 1989}
+# Задание №2
+print(my_dict)
+# Задание №3
+print(my_dict["Ilya"])
+print(my_dict.get("Nadya"))
+# Задание №4
+my_dict.update({"Masha": 2000, "Nastya": 2003})
+# Задание №5
+delete_my_dict = my_dict.pop("Igor")
+print(delete_my_dict)
+# Задание №6
+print(my_dict)
+# Задание №7
+my_set = {1996, 1996, 1997, 1997, "Misha", "Misha", "Misha", "Ilya"}
+# Задание №8
+print(my_set)
+# Задание №9
+my_set.add((28, 27, 2.8, 2.7))
+# Задание №10
+my_set.discard("Misha")
+# Задание №11
+print(my_set)
