453553342bf2d0a0116ee83ba72acceec63177a0
CoffeeWithMi1kk <dbjqip73@gmail.com>
2024-07-04 18:24:14 +0300

    Add files via upload
    Условная конструкция. Операторы if, elif, else.

---
+++ module_2_2.py
@@ -0,0 +1,13 @@
+# Задание №1
+first = input("Введите первое число: ")
+second = input("Введите второе число: ")
+third = input("Введите третье число: ")
+# Задание №2
+if first == second != third or first != second == third or first == third != second:
+    print(2)
+# Задание №3
+elif first == second == third:
+    print(3)
+# Задание №4
+else:
+    print(0)
